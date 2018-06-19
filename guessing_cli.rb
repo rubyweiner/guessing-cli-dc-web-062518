ba5b8124ccb8dd8ccdcbcf0543a27d33d808d78b
Ruby Weiner <rubyweiner796@me.com>
2018-06-19 17:57:49 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,10 +1,9 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   guess = gets.strip
-  if guess.between? (0..6)
-    puts "You guessed the correct number!"
-  else 
-    puts "The computer guessed <number>."
+  if guess == "exit"
+    puts "Goodbye!"
+ end
     
     
 end
