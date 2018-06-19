d27e3fb2bb986cbd5f8dcb893427d1e976d1fc9e
Ruby Weiner <rubyweiner796@me.com>
2018-06-19 18:20:15 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,5 @@
 def run_guessing_game
+  loop do 
   number = rand(1...6)
   puts "Guess a number between 1 and 6."
   guess = gets.chomp
@@ -10,5 +11,5 @@ def run_guessing_game
     puts "You guessed the correct number!"
   end
     
-    
+  end
 end
