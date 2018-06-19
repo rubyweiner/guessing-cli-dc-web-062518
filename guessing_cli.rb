590357c4ccbb1da9984793008cf8e9c11f9af62c
Ruby Weiner <rubyweiner796@me.com>
2018-06-19 18:08:23 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   guess = gets.chomp
   if guess == "exit"
     puts "Goodbye!"
-  elsif guess == number
+  elsif guess.to_i == number
     puts "You guessed the correct number!"
  end
     
